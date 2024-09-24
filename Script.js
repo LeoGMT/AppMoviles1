54995c8ee74aafacf93381289d0774776fef2c1f
Your Name <you@example.com>
2024-09-23 19:39:09 -0600

    18/09/24 Leo

---
+++ Script.js
@@ -0,0 +1,53 @@
+
+/*
+Comentario
+multi
+lineas
+*/
+
+
+// VARIABLES
+/*
+LAS VARIABLES GUARDAN INFORMACION
+QUE PUEDES UTILIZAR 
+*/
+
+let nombre;
+nombre = 'Hola Mundo';
+
+const pi = 3.1416;
+
+var edad = 17;
+
+
+/*Tipos de datos:
+Cadena de texto (String): "hi", 'hi'
+numero (number): 1,2,27
+booleanos (boolean): true, false
+*/
+
+let msj, num, estudiante;
+
+msj = "Hola mundo";
+num = 69;
+estudiante = true;
+
+
+//Operadores mat
+
+let a, b;
+a = 5;
+b = 2;
+
+let suma, resta, multi, div;
+
+suma = a+b;
+resta = a-b;
+multi = a*b;
+div = a/b;
+
+// git add . / git commit -m "18/09/2024 Zetina"
+// git push
+
+
+console.log(nombre,pi,edad,msj,num,estudiante,suma,resta,multi,div);
