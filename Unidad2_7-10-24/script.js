98c1deedecc446f35a4a7b4b19d199a8ae922d54
Your Name <you@example.com>
2024-10-09 20:45:09 -0600

    Leo 9/10/24

--- Unidad2_7-10-24/script.js
+++ Unidad2_7-10-24/script.js
@@ -1,26 +1,27 @@
-function appendToDisplay (value) {
+function appendToDisplay (value) { //Crea una funcion de nombre appendToDisplay que recive un valor para mostrarlo en el display
     
-    document.getElementById('display').value += value;
+    document.getElementById('display').value += value;//metodo utilizado para obtener un elemento por base de un id
 }
 
- function clearDisplay() {
-    document.getElementById('display').value ='';
+ function clearDisplay() { //Crea una funcion de nombre clearDisplay para borrar el contenido del display
+    document.getElementById('display').value ='';//metodo utilizado para obtener un elemento por base de un id
  }
     
     
-function deleteLast() {
+function deleteLast() {//Crea una funcion de nombre deleteLast que borra el ultimo digito del display
     
-    let displayValue = document.getElementById('display').value;
-    document.getElementById('display').value = displayValue.slice(0, -1);
+    
+    let displayValue = document.getElementById('display').value;//Se va recorriendo el arreglo que contiene el display
+    document.getElementById('display').value = displayValue.slice(0, -1);//metodo para borra el ultimo digito del display
 }
     
- function calculateResult() {
+ function calculateResult() {//Crea una funcion de nombre deleteLast que borra el ultimo digito del display
     
-    try {
+    try {//Funcion para encontrar errores y dar un resultado
     
-    let result = eval(document.getElementById('display').value);
+    let result = eval(document.getElementById('display').value);//Se va recorriendo el arreglo que contiene el display
     document.getElementById('display').value = result;
     } catch (error) {
-         alert('Expresión inválida');
+         alert('Expresión inválida');//Manda una alerta de expresion invalida
     }
 }
